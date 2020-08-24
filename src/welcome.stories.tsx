dea040a8af25bd8b938ff47947a45d0a6200efe1
AlmostSire <18810759795@163.com>
2020-08-24 16:10:25 +0800

    welcome

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -6,7 +6,7 @@ storiesOf('Welcome page', module)
         return (
             <>
                 <h1>欢迎来到 vikingship 组件库</h1>
-                <p>almostlover 为 Web 应用提供了丰富的基础 UI 组件，我们还将持续探索企业级应用的最佳 UI 实践。除了官方组件，我们也提供了社区精选组件作为必要的补充。</p>
+                <p>almostlover 为 Web 应用提供了丰富的基础 UI 组件</p>
                 <h3>安装试试</h3>
                 <code>
                     npm install almostlover --save
